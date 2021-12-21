9783c65c660ecb35cd1d6201c5e9aa1791451357
Vquirino <v.quirino80@gmail.com>
2021-12-21 10:08:39 -0300

    começo dos testes unitários

--- minhasfinancas/src/main/java/com/vquirino/minhasfinancas/model/entity/Usuario.java
+++ minhasfinancas/src/main/java/com/vquirino/minhasfinancas/model/entity/Usuario.java
@@ -9,7 +9,6 @@ import javax.persistence.Table;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Setter;
 
 @Entity
 @Table(name = "usuario", schema = "financas")
--- minhasfinancas/src/main/java/com/vquirino/minhasfinancas/model/repository/UsuarioRepository.java
+++ minhasfinancas/src/main/java/com/vquirino/minhasfinancas/model/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.vquirino.minhasfinancas.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vquirino.minhasfinancas.model.entity.Usuario;
